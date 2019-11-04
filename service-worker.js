230aee2add9da6e9ac7e855607c4bbe3d117cf07
Alexander Ivanov <random1911@gmail.com>
2019-11-04 15:45:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,16 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hooks-mst-example/precache-manifest.44308f961821ee4af3a3e2e829bac0cc.js"
+  "/react-hooks-mst-example/precache-manifest.b708c5f0651443ddf7749bd1ea97d877.js"
 );
 
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -35,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/react-hooks-mst-example/index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/]
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-hooks-mst-example/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
